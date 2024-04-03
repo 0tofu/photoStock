c66f7931a883608b6b6198c02ae11c80adeddb1d
Motoki Kobayashi <otofu.xxx@gmail.com>
2024-04-03 20:43:01 +0900

    サブディレクトリに配置した画像パスが正しく設定できていない問題を修正

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,3 @@
-import * as path from 'path'
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
 import { glob } from 'glob'
@@ -10,7 +9,7 @@ const PhotoList: FC<{ images: string[] }> = (props: { images: string[] }) => {
   return (
     <ul className="container m-auto gap-4 grid grid-cols-none sm:grid-cols-3 lg:grid-cols-5">
       {props.images.map((image) => {
-        const imagePath = `/photos/${path.basename(image)}`
+        const imagePath = image.replace(/^\.\.\//, '/')
         return <li><a href={imagePath}><img src={imagePath}/></a></li>
       })}
     </ul>
@@ -28,7 +27,7 @@ app.use('*', renderer)
 app.use('*', twindInjector())
 
 app.get('/', async (c) => {
-  const images = await glob('../photos/**/*.jpg')
+  const images = await glob('../photos/**/*.{jpg,jpeg}')
   return c.render(<PhotoList images={images} />)
 })
 
